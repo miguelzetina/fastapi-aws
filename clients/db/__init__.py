4abaa6d24175e808bd7d7e2e4e5de1e258bab42a
Miguel Angel Zetina Zetina <miguel.zetina@storicard.com>
2022-12-29 11:35:33 -0600

    add motor and define client

---
+++ clients/db/__init__.py
@@ -0,0 +1,81 @@
+from typing import List
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+from src.config import settings
+
+
+mongo_client: AsyncIOMotorClient
+
+db_url = settings.DB_URL
+db_name = settings.DB_NAME
+
+
+def get_db() -> AsyncIOMotorDatabase:
+    return AsyncIOMotorClient(db_url)[db_name]
+
+
+# Record Retrieval Helper Functions
+async def get_record(collection: str, query:dict) -> dict:
+    # if object has a null id as well as _id
+    # then change to get_db()[collection].find_one(query, {'id': False})
+    return await get_db()[collection].find_one(query)
+
+
+async def get_record_count(collection: str, query:dict) -> dict:
+    return await get_db()[collection].count_documents(query)
+
+
+async def agregate(collection: str, pipeline:list[dict]) -> dict:
+    return get_db()[collection].aggregate(pipeline=pipeline, allowDiskUse=True)
+
+
+async def get_records(collection: str, query:dict, projection: dict = None):
+    return get_db()[collection].find(query, projection)
+
+
+async def get_records_with_pagination(collection: str,
+                                      query:dict,
+                                      pagination_info: dict,
+                                      limit: int) -> dict:
+    return get_db()[collection]\
+            .find(query)\
+            .skip(pagination_info['skip'])\
+            .limit(limit)
+
+
+# Record Creation and Updating Helper Functions
+async def create_record(collection: str,
+                        query: dict) -> dict:
+    if '_id' in query:
+        del query['_id']
+    return await get_db()[collection].insert_one(query)
+
+
+async def update_record(collection: str,
+                        object_query: dict,
+                        update_query: dict) -> dict:
+    if '_id' in update_query:
+        del update_query['_id']
+    return await get_db()[collection].update_one(object_query, update_query)
+
+
+async def get_and_update_record(collection: str,
+                                object_query: dict,
+                                update_query: dict) -> dict:
+    if '_id' in update_query:
+        del update_query['_id']
+    return await get_db()[collection].find_one_and_update(object_query,
+                                                          update_query)
+
+
+# Record Deletion Helper Functions
+async def delete_record(collection: str,
+                        query: dict) -> dict:
+    return await get_db()[collection].delete_one(query)
+
+
+async def delete_all_records(collection:str,
+                             query: dict) -> dict:
+    return await get_db()[collection].delete_many(query)
+
